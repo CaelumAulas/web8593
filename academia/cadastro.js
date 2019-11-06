d525bbb40573e099da29e729c35d2920f4db9033
Artur Diniz Adam <artur.daz@gmail.com>
2019-11-05 22:11:37 -0300

    Funcao factory de data

--- academia/cadastro.js
+++ academia/cadastro.js
@@ -8,6 +8,29 @@ function validaData(data) {
     return listaMatchData !== null
 }
 
+// Fábrica de datas
+// Factory Function
+// Função Factory
+function extraiData(data) {
+    var padraoData = /(\d{2})\/(\d{2})\/(\d{4})/
+    var listaMatchData = data.match(padraoData)
+    
+    if(listaMatchData === null) {
+        return null
+    }
+
+    var dia = listaMatchData[1]
+    var mes = listaMatchData[2]
+    var ano = listaMatchData[3]
+
+    // Objeto Literal
+    return {
+        "dia": dia, 
+        "mes": mes,
+        "ano":  ano
+    }
+}
+
 function validaCPF(cpf) {
     var padraoCPF = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
     var listaMatchCPF = cpf.match(padraoCPF)
@@ -21,14 +44,19 @@ function cadastro() {
     var altura = $inputAltura.value
     var peso = $inputPeso.value
 
-    var idade = calculaIdade()
+    var data = extraiData(dataNascimento)
 
-    var dataValida = validaData(dataNascimento)
+    var dataValida = data !== null 
     var cpfValido = validaCPF(cpf)
     
     var estaValido = dataValida && cpfValido
     
     if(estaValido) {
+        var dia = data.dia
+        var mes = data.mes
+        var ano = data.ano
+        var idade = calculaIdade(dia, mes, ano)
+        
         // salvar
 
         // exibir na página
